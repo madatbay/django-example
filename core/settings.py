046645bd561b08a8da4388d983f2a4afac7201de
Madat Bay <46858323+madatbay@users.noreply.github.com>
2021-07-19 19:01:04 +0400

    db name changed

--- core/settings.py
+++ core/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sample-database', 
+        'NAME': 'sampledatabase', 
         'USER': 'dbuser', 
         'PASSWORD': 'dbpass123',
         'HOST': 'localhost', 
